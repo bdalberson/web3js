3c2d84ab6a2a451d942e0eaa0b1b9f3b9730ca47
bdalberson <Brian.alberson@gmail.com>
2023-06-13 15:18:32 -0700

    figures

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-var mainnet_url = "https://mainnet.infura.io/v3/7dcc54ef0fd743bb808a01f7b421a89c"
+
+
+const Web3 = require('web3');
+console.log(web3.version)
+// var web3 = new Web3('https://mainnet.infura.io/v3/7dcc54ef0fd743bb808a01f7b421a89c');
+// Web3.givenProvider
+// web3.eth.net.isListening()
+//   .then(console.log)  // true if connected
+//   .catch(console.error);
+
